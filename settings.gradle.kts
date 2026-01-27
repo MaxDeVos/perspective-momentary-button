ebb6a347fd9035eb848531e8c55aa30833f48d61
Max Devos <max@maxdevos.com>
2026-01-27 00:27:56 -0600

    Prune Gradle repos down to gradlePluginPortal &  nexus/public

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,24 @@
 // this file configures settings for the gradle build tools, as well as the project structure.
 // Generally this doesn't need to be altered unless you are adding/removing sub-projects.
+rootProject.name = "perspective-momentary-button"
 
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven {
-            url = uri("https://nexus.inductiveautomation.com/repository/public/")
-        }
+        maven("https://nexus.inductiveautomation.com/repository/public/")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
-        maven { url = uri("https://nexus.inductiveautomation.com/repository/public/") }
-        maven { url = uri("https://nexus.inductiveautomation.com/repository/inductiveautomation-thirdparty/") }
-        maven { url = uri("https://nexus.inductiveautomation.com/repository/inductiveautomation-releases/") }
-        maven { url = uri("https://nexus.inductiveautomation.com/repository/inductiveautomation-snapshots/") }
-        mavenCentral()
-        google()
+        maven("https://nexus.inductiveautomation.com/repository/public/")
     }
 }
 
-rootProject.name = "perspective-momentary-button"
-
 // link up our subprojects as part of this multi-project build.  Add/remove subprojects gradle path notation.
-include(":common", ":gateway", ":designer", ":web")
+include(
+    ":common",
+    ":gateway",
+    ":designer",
+    ":web"
+)
