465e2a9fa2d5f4ac147ca854d5bc477522769f35
Max Devos <max@maxdevos.com>
2026-01-31 20:11:13 -0600

    Add workaround for broken javadocs in IDEA

--- build-logic/src/main/kotlin/org/imdc/conventions/modl-jvm-component.gradle.kts
+++ build-logic/src/main/kotlin/org/imdc/conventions/modl-jvm-component.gradle.kts
@@ -6,6 +6,7 @@ import libs
 plugins {
     id("org.imdc.conventions.modl-component")
     `java-library`
+    idea
 }
 
 java {
@@ -25,4 +26,15 @@ configure<SpotlessExtension> {
     kotlinGradle {
         ktlint()
     }
+}
+
+// Explicitly instruct IDEA *not* to download sources as workaround for a bug/flaw in IDEA's javadoc integration.
+// It seems that if IDEA cannot resolve the sources JAR for a given dependency, it won't bother trying to resolve the javadocs either.
+// This behavior is troublesome with respect to IA packages, which never contain sources, but often contain javadocs.
+// Unfortunately, this also means no source resolution for source-available non-IA packages like Jetty, but it doesn't
+// truly matter since IA excludes third-party sources and javadocs from their Nexus repositories, which we are using
+// exclusively for non-plugin dependency resolution. I do not know why they do this, but there is probably a good reason.
+idea.module {
+    isDownloadJavadoc = true
+    isDownloadSources = false
 }
